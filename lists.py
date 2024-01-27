6f3f96e95c6d331082506605d646ef49d2979b4a
teo <tkatsigiannis@gmail.com>
2024-01-27 18:54:02 +0200

    Initial Commit

---
+++ lists.py
@@ -0,0 +1,22 @@
+list = ['Teo', 'Maria', 'Christina', 'Antonis', 'Georgos', 'Stefanos', 'Leonidas', 'Spyros']
+ages = [52, 16, 17, 15, 16, 16, 16, 16]
+
+print(f"Hello {list[0]} you are {ages[0]} years old, I am your first program")
+print(f"Hello {list[1]} you are {ages[1]} years old, I am your first program")
+print(f"Hello {list[2]} you are {ages[2]} years old, I am your first program")
+print(f"Hello {list[3]} you are {ages[3]} years old, I am your first program")
+print(f"Hello {list[4]} you are {ages[4]} years old, I am your first program")
+print(f"Hello {list[5]} you are {ages[5]} years old, I am your first program")
+print(f"Hello {list[6]} you are {ages[6]} years old, I am your first program")
+print(f"Hello {list[7]} you are {ages[7]} years old, I am your first program")
+list = ['Teo', 'Maria', 'Christina', 'Antonis', 'Georgos', 'Stefanos', 'Leonidas', 'Spyros']
+ages = [52, 16, 17, 15, 16, 16, 16, 16]
+
+print(f"Hello {list[0]} you are {ages[0]} years old, I am your first program")
+print(f"Hello {list[1]} you are {ages[1]} years old, I am your first program")
+print(f"Hello {list[2]} you are {ages[2]} years old, I am your first program")
+print(f"Hello {list[3]} you are {ages[3]} years old, I am your first program")
+print(f"Hello {list[4]} you are {ages[4]} years old, I am your first program")
+print(f"Hello {list[5]} you are {ages[5]} years old, I am your first program")
+print(f"Hello {list[6]} you are {ages[6]} years old, I am your first program")
+print(f"Hello {list[7]} you are {ages[7]} years old, I am your first program")
---
+++ variables.py
@@ -0,0 +1,3 @@
+myname = 'Teo'
+print(f"My name is {myname}")
+print(f"Hello {myname} I am your first computer program")
